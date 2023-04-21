bc97e47e11bb47d4e4676b0f45b6aeea69c937a0
Felix <github@kakrow.me>
2023-04-21 18:10:27 +0200

    Updates Opcache reset

--- helpers/reset_opcache.php
+++ helpers/reset_opcache.php
@@ -1,18 +1,6 @@
 <?php
 header("Content-type: application/json; charset=utf-8");
 
-if ($_SERVER["REMOTE_ADDR"] !== "127.0.0.1") {
-    echo $_SERVER["REMOTE_ADDR"];
-    echo json_encode(
-        [
-            'status' => "nope."
-        ]
-    );
-    die;
-}
-
-ob_start();
-$result =  opcache_reset();
 echo json_encode([
     'status' => ($result ? 'success' : 'error'),
     'message' => ($result ? 'opcache was reset' : 'opcache not reset'),
