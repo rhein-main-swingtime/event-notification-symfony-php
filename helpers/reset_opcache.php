2618e5c06d109db27856c6c179d5c6a07d733159
Felix <github@kakrow.me>
2023-04-21 18:58:21 +0200

    Fixes opcache clear

--- helpers/reset_opcache.php
+++ helpers/reset_opcache.php
@@ -1,6 +1,8 @@
 <?php
 header("Content-type: application/json; charset=utf-8");
 
+ob_start();
+$result =  opcache_reset();
 echo json_encode([
     'status' => ($result ? 'success' : 'error'),
     'message' => ($result ? 'opcache was reset' : 'opcache not reset'),
